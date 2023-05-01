572a8e7c154fa8cc63fdeaced3e5e6f094497dc5
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-05-01 13:35:26 +0530

    learned bubble sorting

--- Sorting/bubble_sorting.js
+++ Sorting/bubble_sorting.js
@@ -1,17 +1,18 @@
 const unsort_arr = [10, 12, 22, 32, 18, 4];
 
-function bubble_sort() {
-  for (let i = unsort_arr.length - 1; i > 0; i--) {
-    let max = i;
-    for (let j = i - 1; j >= 0; j--) {
-      if (unsort_arr[j] > unsort_arr[max]) {
-        max = j;
-      };
-    };
-    const tmp = unsort_arr[max];
-    unsort_arr[max] = unsort_arr[i];
-    unsort_arr[i] = tmp;
-  };
-};
-bubble_sort();
-console.log(unsort_arr); // -> [4,10,12,18,22,32];
+function bubbleSort() {
+  let swaped = true;
+  while (swaped) {
+    swaped = false;
+    for (let i = 0; i < unsort_arr.length - 1; i++) {
+      if (unsort_arr[i] > unsort_arr[i + 1]) {
+        const tmp = unsort_arr[i];
+        unsort_arr[i] = unsort_arr[i + 1];
+        unsort_arr[i + 1] = tmp;
+        swaped = true;
+      }
+    }
+  }
+}
+bubbleSort();
+console.log(unsort_arr);
