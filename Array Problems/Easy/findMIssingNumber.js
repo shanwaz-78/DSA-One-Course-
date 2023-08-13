a40dd49e31a4abaddd9bf03e444810df1b2dc413
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-08-13 19:00:08 +0530

    Solved FindMissing nUmber Problem

---
+++ Array Problems/Easy/findMIssingNumber.js
@@ -0,0 +1,10 @@
+function find_missing_number(arr, n) {
+  let missing_numbers = [];
+  for (let i = 1; i <= n; i++) {
+    if (!arr.includes(i)) {
+      missing_numbers.push(i);
+    }
+  }
+  return missing_numbers;
+}
+console.log(find_missing_number([1, 2, 4, 5], 5));
