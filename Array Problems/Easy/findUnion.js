6f4fc8ea7767f544e7fd51a7e348ac13669eae5f
shanwaz-78 <111566952+shanwaz-78@users.noreply.github.com>
2023-12-11 19:28:46 +0530

    Update findUnion.js

--- Array Problems/Easy/findUnion.js
+++ Array Problems/Easy/findUnion.js
@@ -5,7 +5,7 @@ function findUnion(arr1, arr2) {
       union.add(key);
     }
   }
-  return arr1.concat(Array.from(union)); // IF we use ... here then the time cmplexity of this will be O(n2);
+  return arr1.concat(Array.from(union));
 }
 console.log(
   findUnion([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [2, 3, 4, 4, 5, 11, 12])
